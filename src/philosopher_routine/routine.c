3b7dcc5dd73ede9c22d314dcf5e2b7276f8e48b8
Mohamed Haddi <mhaddi@e2r6p11.1337.ma>
2021-12-22 18:13:14 +0100

    replace unecessary re-access to start_time

--- src/philosopher_routine/routine.c
+++ src/philosopher_routine/routine.c
@@ -6,20 +6,24 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 16:21:34 by mhaddi            #+#    #+#             */
-/*   Updated: 2021/12/22 16:55:17 by mhaddi           ###   ########.fr       */
+/*   Updated: 2021/12/22 18:13:06 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
 
-void	set_thread_start_time(t_thread_data *philosopher_data)
+size_t	set_thread_start_time(t_thread_data *philosopher_data)
 {
+	size_t	start_time;
+
 	pthread_mutex_lock(&philosopher_data->locks->start_time_lock);
 	pthread_mutex_lock(&philosopher_data->locks->latest_meal_time_lock);
 	philosopher_data->start_time = get_time();
-	philosopher_data->latest_meal_time = philosopher_data->start_time;
+	start_time = philosopher_data->start_time;
+	philosopher_data->latest_meal_time = start_time;
 	pthread_mutex_unlock(&philosopher_data->locks->latest_meal_time_lock);
 	pthread_mutex_unlock(&philosopher_data->locks->start_time_lock);
+	return (start_time);
 }
 
 void	*philosopher_routine(void *thread_data)
@@ -29,8 +33,7 @@ void	*philosopher_routine(void *thread_data)
 	pthread_mutex_t	*forks_locks;
 
 	philosopher_data = (t_thread_data *)thread_data;
-	set_thread_start_time(philosopher_data);
-	start_time = philosopher_data->start_time;
+	start_time = set_thread_start_time(philosopher_data);
 	forks_locks = philosopher_data->locks->forks;
 	if (philosopher_data->thread_number % 2)
 		ft_usleep(philosopher_data->input_data.time_to_eat);
